25f8288ecaa90a8bb5a5ede236e6240d0d5f1435
Aboelelaa <120112034+Aboelelaa@users.noreply.github.com>
2026-02-20 14:31:28 +0200

    Remove Mapbox token or placeholder

--- public/assets/js/map-leafleft.js
+++ public/assets/js/map-leafleft.js
@@ -1,83 +1,83 @@
+// // $(function() {
+// // 	'use strict';
+// // 	// Leftlet Maps
+// // 	var mymap = L.map('leaflet1').setView([51.505, -0.09], 13);
+// // 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+// // 		maxZoom: 18,
+// // 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap<\/a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA<\/a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox<\/a>',
+// // 		id: 'mapbox.streets'
+// // 	}).addTo(mymap);
+// // 	// Adding a Popup
+// // 	var mymap2 = L.map('leaflet2').setView([51.505, -0.09], 13);
+// // 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+// // 		maxZoom: 18,
+// // 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap<\/a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA<\/a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox<\/a>',
+// // 		id: 'mapbox.streets'
+// // 	}).addTo(mymap2);
+// // 	L.marker([51.5, -0.09]).addTo(mymap2).bindPopup("<b>Hello world!<\/b><br />I am a popup.").openPopup();
+// // 	// Adding a Circle
+// // 	var mymap3 = L.map('leaflet3').setView([51.505, -0.09], 13);
+// // 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+// // 		maxZoom: 18,
+// // 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap<\/a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA<\/a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox<\/a>',
+// // 		id: 'mapbox.streets'
+// // 	}).addTo(mymap3);
+// // 	L.circle([51.508, -0.11], {
+// // 		color: 'red',
+// // 		fillColor: '#f03',
+// // 		fillOpacity: 0.5,
+// // 		radius: 500
+// // 	}).addTo(mymap3);
+// // });
+
+
+
+
+
 // $(function() {
-// 	'use strict';
-// 	// Leftlet Maps
-// 	var mymap = L.map('leaflet1').setView([51.505, -0.09], 13);
-// 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-// 		maxZoom: 18,
-// 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap<\/a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA<\/a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox<\/a>',
-// 		id: 'mapbox.streets'
-// 	}).addTo(mymap);
-// 	// Adding a Popup
-// 	var mymap2 = L.map('leaflet2').setView([51.505, -0.09], 13);
-// 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-// 		maxZoom: 18,
-// 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap<\/a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA<\/a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox<\/a>',
-// 		id: 'mapbox.streets'
-// 	}).addTo(mymap2);
-// 	L.marker([51.5, -0.09]).addTo(mymap2).bindPopup("<b>Hello world!<\/b><br />I am a popup.").openPopup();
-// 	// Adding a Circle
-// 	var mymap3 = L.map('leaflet3').setView([51.505, -0.09], 13);
-// 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-// 		maxZoom: 18,
-// 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap<\/a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA<\/a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox<\/a>',
-// 		id: 'mapbox.streets'
-// 	}).addTo(mymap3);
-// 	L.circle([51.508, -0.11], {
-// 		color: 'red',
-// 		fillColor: '#f03',
-// 		fillOpacity: 0.5,
-// 		radius: 500
-// 	}).addTo(mymap3);
+//     'use strict';
+
+//     // خزن التوكن في متغير (ممكن بعدين تجيبه من ملف config أو env)
+//     // var mapboxToken = window.MAPBOX_TOKEN; // <-- استخدم التوكن من ملف config.js
+
+//     // Leaflet Maps
+//     var mymap = L.map('leaflet1').setView([51.505, -0.09], 13);
+//     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxToken, {
+//         maxZoom: 18,
+//         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+//                      '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+//                      'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//         id: 'mapbox.streets'
+//     }).addTo(mymap);
+
+//     // Adding a Popup
+//     var mymap2 = L.map('leaflet2').setView([51.505, -0.09], 13);
+//     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxToken, {
+//         maxZoom: 18,
+//         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+//                      '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+//                      'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//         id: 'mapbox.streets'
+//     }).addTo(mymap2);
+
+//     L.marker([51.5, -0.09]).addTo(mymap2)
+//       .bindPopup("<b>Hello world!</b><br />I am a popup.")
+//       .openPopup();
+
+//     // Adding a Circle
+//     var mymap3 = L.map('leaflet3').setView([51.505, -0.09], 13);
+//     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxToken, {
+//         maxZoom: 18,
+//         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+//                      '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+//                      'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//         id: 'mapbox.streets'
+//     }).addTo(mymap3);
+
+//     L.circle([51.508, -0.11], {
+//         color: 'red',
+//         fillColor: '#f03',
+//         fillOpacity: 0.5,
+//         radius: 500
+//     }).addTo(mymap3);
 // });
-
-
-
-
-
-$(function() {
-    'use strict';
-
-    // خزن التوكن في متغير (ممكن بعدين تجيبه من ملف config أو env)
-    var mapboxToken = ''; // <-- حط التوكن هنا محلياً بس، لا ترفعه على GitHub
-
-    // Leaflet Maps
-    var mymap = L.map('leaflet1').setView([51.505, -0.09], 13);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxToken, {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-                     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        id: 'mapbox.streets'
-    }).addTo(mymap);
-
-    // Adding a Popup
-    var mymap2 = L.map('leaflet2').setView([51.505, -0.09], 13);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxToken, {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-                     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        id: 'mapbox.streets'
-    }).addTo(mymap2);
-
-    L.marker([51.5, -0.09]).addTo(mymap2)
-      .bindPopup("<b>Hello world!</b><br />I am a popup.")
-      .openPopup();
-
-    // Adding a Circle
-    var mymap3 = L.map('leaflet3').setView([51.505, -0.09], 13);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxToken, {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-                     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        id: 'mapbox.streets'
-    }).addTo(mymap3);
-
-    L.circle([51.508, -0.11], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 500
-    }).addTo(mymap3);
-});
